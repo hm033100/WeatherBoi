9cc61eff5addf4c0d936082dbf444494891d7ce8
Nathan Ford <NathanFord68@gmail.com>
2020-02-06 22:31:04 -0700

    templates set up and started the form for the login page

--- src/model/User.java
+++ src/model/User.java
@@ -5,32 +5,59 @@ import javax.faces.bean.ViewScoped;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.hibernate.validator.constraints.Email;
+
 @ManagedBean
 @ViewScoped
 public class User {
 	
+	private int id;
+	
+	@NotNull(message = "Please enter an email. This is a required field.")
+	@Email(message = "This field must be an email.")
+	private String email;
+	
 	@NotNull(message = "Please enter a username. This is a required field.")
 	@Size(min = 4, max = 15)
-	private String username = "";
+	private String username;
 
 	@NotNull(message = "Please enter a password. This is a required field.")
 	@Size(min = 4, max = 15)
-	private String password = "";
+	private String password;
 
-	private int id;
+
 
 	public User() {
 		super();
+		this.id = -1;
+		this.email = "";
 		this.username = "";
 		this.password = "";
-		this.id = -1;
 	}
 	
-	public User(String username, String password, int id) {
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public User(int id, String email, String username, String password) {
 		super();
+		this.id = id;
+		this.email = email;
 		this.username = username;
 		this.password = password;
-		this.id = id;
+		
 	}
 
 	public String getUsername() {
